a273ab13884a2d0d5d6f77a330b0b6237a266ad4
OleksandrKyrylenko <110344139+OleksandrKyrylenko@users.noreply.github.com>
2022-12-08 10:37:30 +0200

    lessons_22

---
+++ lessons__22/js/script.js
@@ -0,0 +1,19 @@
+// 'use strict' - 
+// директива яка змушує код оброблятися у строгому режимі. 
+// Без цієї установки код обробляється у необмеженому режимі.
+'use strict'
+console.log('Я вивчаю JS');
+console.log('Учим'); console.log('JS')
+// ==========================================================
+
+let userColorEye = '#045813';
+console.log(userColorEye);
+
+
+let user;
+let userName = 'Вася';
+user = userName;
+console.log(userName);
+console.log(user);
+
+// ============================================================
